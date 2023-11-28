eff0723149082a0a1e7d2d23b120893ce95d70b5
Juan Silveira <juansilveira@gmail.com>
2023-11-28 12:24:01 -0300

    add menu component and bootstrap import

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,36 @@
+const NavBar = () => {
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src="./logo192.png" alt="logo" height={"30px"} />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Início</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Eletrônicos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Esportes e Lazer</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Casa e Jardim</a>
+                            </li>
+                        </ul>
+                        <a className="btn btn-primary" href="#">Login</a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>
 );
