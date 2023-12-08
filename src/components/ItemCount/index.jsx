a2dddcd7c9b69f7f3f00509c53197b26299040ef
Juan Silveira <juan.silveira@starmountain.pt>
2023-12-08 10:07:07 -0300

    feat: ItemCount

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,18 +1,28 @@
+import React, { useState } from "react";
+
 const ItemCount = (props) => {
 
-    const stock = props.stock;
-    const initial = props.initial;
+    const stock = parseInt(props.stock);
+    const initial = parseInt(props.initial);
+    const [count, setCount] = useState(initial);
 
-    
+    function removeItem() {
+        if(count > initial)
+        return setCount(count - 1)
+    }
 
+    function addItem() {
+        if(count < stock)
+        return setCount(count + 1)
+    }
 
     return (
         <>
-            <div className="form-group">
+            <div className="form-group border mt-3 py-2">
                 <p className="text-center">Camiseta Preta</p>
-                <button className="btn btn-outline-primary btn-icon" id="remove"><i className="bi bi-dash"></i></button>
-                <input type="number" placeholder="1"/>
-                <button className="btn btn-outline-primary btn-icon" id="add"><i className="bi bi-plus"></i></button>
+                <button className="btn btn-outline-primary btn-icon" id="remove" onClick={ removeItem }><i className="bi bi-dash"></i></button>
+                <input className="text-center mx-2" type="text" placeholder="1" value={ count } style={{maxWidth:"50px"}}/>
+                <button className="btn btn-outline-primary btn-icon" id="add" onClick={ addItem }><i className="bi bi-plus"></i></button>
             </div>
         </>
     )
